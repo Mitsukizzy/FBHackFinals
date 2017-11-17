016ce7194fca4edfe8294b50c2cf86fb99bf9102
Brandon Cen <bcen96@gmail.com>
2017-11-16 17:32:49 -0800

    modularize scripts

---
+++ FBHackFinals/Assets/Scripts/FirebaseModule2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseModule2 : FirebaseTest {
+
+	// Use this for initialization
+	void Start () {
+		//SaveScore (12);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void SaveScore(int score) {
+		base.SaveIntoTable ("module2", score);
+	}
+}
